afdc1296e4711aaa80213e280bdedbb948fdbde8
younghz <younghaizhi@gmail.com>
2015-06-06 16:17:53 +0800

    add post

--- _posts/genTemplate4Blog.py
+++ _posts/genTemplate4Blog.py
@@ -16,7 +16,7 @@ fileName = dateString + '-' + title + '.md'
 print '文件名称:' + fileName
 
 if (os.path.exists(fileName)):
-    print '文件名已经存在，你怎么能写两篇一样的文章？'
+    print '\n文件已经存在，你怎么能写两篇一样的文章？'
     # end
     sys.exit(0)
 
@@ -24,10 +24,13 @@ if (os.path.exists(fileName)):
 print '\n创建文件' + fileName + '成功'
 toBeCreateFile = open(fileName, 'w');
 toBeWriteToFile = '''--- \nlayout: post \ntitle: "title name" \ntagline: "child title" \ndescription: "" \n\
-category: book \ntags: [book] \n--- \n{% include JB/setup %'''
+category: book \ntags: [book] \n--- \n{% include JB/setup %}'''
 
 try:
     toBeCreateFile.write(toBeWriteToFile)
+except Exception,e:
+    print '\n写文件异常'
+    print e.message
 
 finally:
     toBeCreateFile.close()
